b682eb2a6c7dbcc605ba67723c51a8c5e20c3891
Basant Sharma <basant.sharma@ebutor.com>
2020-07-31 16:42:53 +0530

    Basic application skelton with global constant & dumny data

---
+++ lib/models/product.dart
@@ -0,0 +1,67 @@
+class Product {
+  final int id, price;
+  final String title, description, image;
+
+  Product({this.id, this.price, this.title, this.description, this.image});
+}
+
+// List of products
+List<Product> products = [
+  Product(
+    id: 1,
+    price: 100,
+    title: "Classic leather arm chair",
+    image: "assests/images/Item_1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  ),
+  Product(
+    id: 1,
+    price: 500,
+    title: "Classic leather arm chair",
+    image: "assests/images/Item_1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  ),
+  Product(
+    id: 1,
+    price: 300,
+    title: "Classic leather arm chair",
+    image: "assests/images/Item_1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  ),
+  Product(
+    id: 1,
+    price: 250,
+    title: "Classic leather arm chair",
+    image: "assests/images/Item_1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  ),
+  Product(
+    id: 1,
+    price: 88,
+    title: "Classic leather arm chair",
+    image: "assests/images/Item_1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  ),
+  Product(
+    id: 1,
+    price: 100,
+    title: "Classic leather arm chair",
+    image: "assests/images/Item_1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  ),
+  Product(
+    id: 1,
+    price: 999,
+    title: "Classic leather arm chair",
+    image: "assests/images/Item_1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  ),
+  Product(
+    id: 1,
+    price: 111,
+    title: "Classic leather arm chair",
+    image: "assests/images/Item_1.png",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  ),
+
+]
